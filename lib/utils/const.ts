0f0e33e4a458f0c30097f3ee69cad1353635cc95
aoobao <aoobao@foxmail.com>
2023-03-11 12:57:50 +0800

    add

--- lib/utils/const.ts
+++ lib/utils/const.ts
@@ -2,7 +2,13 @@
 // export const Symbol_Scale = Symbol('scale')
 // export const Symbol_theta = Symbol('theta')
 
-export const GET_MATRIX = Symbol('matrix')
+/**自身矩阵 */
+export const MATRIX_SELF = Symbol('matrix')
+/**父节点 */
+export const PARENT = Symbol('parent')
+/**世界矩阵 */
+export const MATRIX_WORLD = Symbol('matrix_world')
 
 export const DEFAULT_Z_INDEX = 100
 
+export const RENDER = Symbol('render')
